964e5470d44d6e38db815dc30a5954426f5d8ef1
Christian <christian.walter@outlook.at>
2020-11-27 01:58:54 +0100

    another construction [do not use]

--- ml.py
+++ ml.py
@@ -5,6 +5,9 @@ Machine Learning file for training and evaluating the model
 import numpy as np
 import matplotlib.pyplot as plt
 
+import logging
+import time
+
 import os
 import torch
 import yaml
@@ -14,168 +17,7 @@ from skimage.util.shape import view_as_windows
 # my stuff
 from common import *
 from plots import *
-from conv_nets import *
-
-import logging
-import time
-
-
-
-
-
-
-
-
-def train_nn(model, x_train, y_train, x_val, y_val, classes, nn_arch, num_epochs=2, lr=1e-3, param_str='nope'):
-  """
-  train the neural network thing
-  """
-
-  # Loss Criterion
-  criterion = torch.nn.CrossEntropyLoss()
-
-  # create optimizer
-  #optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.5)
-  optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
-  print("\n--Training starts:")
-
-  # collect losses over epochs
-  train_loss = np.zeros(num_epochs)
-  val_loss = np.zeros(num_epochs)
-  val_acc = np.zeros(num_epochs)
-
-  # start time
-  start_time = time.time()
-
-  # epochs
-  for epoch in range(num_epochs):
-
-    # cumulated loss
-    cum_loss = 0.0
-
-    # TODO: do this with loader function from pytorch (maybe or not)
-    # fetch data samples
-    for i, (x, y) in enumerate(zip(x_train, y_train)):
-
-      # zero parameter gradients
-      optimizer.zero_grad()
-
-      # forward pass o:[b x c]
-      o = model(x)
-
-      # loss
-      loss = criterion(o, y)
-
-      # backward
-      loss.backward()
-
-      # optimizer step - update params
-      optimizer.step()
-
-      # loss update
-      cum_loss += loss.item()
-
-      # batch loss
-      train_loss[epoch] += cum_loss
-
-      # print some infos, reset cum_loss
-      cum_loss = print_train_info(epoch, i, cum_loss, k_print=y_train.shape[0] // 10)
-
-    # valdiation
-    val_loss[epoch], val_acc[epoch], _ = eval_nn(model, x_val, y_val, classes, logging_enabled=False)
-
-    # TODO: Early stopping if necessary
-    
-  print('--Training finished')
-
-  # log time
-  logging.info('Traning on arch: {}  time: {}'.format(param_str, s_to_hms_str(time.time() - start_time)))
-
-  return model, train_loss, val_loss, val_acc
-
-
-def eval_nn(model, x_batches, y_batches, classes, z_batches=None, logging_enabled=True, calc_cm=False, verbose=False):
-  """
-  evaluation of nn
-  """
-
-  # Loss Criterion
-  criterion = torch.nn.CrossEntropyLoss()
-
-  # init
-  correct, total, eval_loss, cm = 0, 0, 0.0, None
-  y_all, y_hat_all = np.empty(shape=(0), dtype=y_batches.numpy().dtype), np.empty(shape=(0), dtype=y_batches.numpy().dtype)
-
-  # no gradients for eval
-  with torch.no_grad():
-
-    # load data
-    for i, (x, y) in enumerate(zip(x_batches, y_batches)):
-
-      # classify
-      o = model(x)
-
-      # loss
-      eval_loss += criterion(o, y)
-
-      # prediction
-      _, y_hat = torch.max(o.data, 1)
-
-      # add total amount of prediction
-      total += y.size(0)
-
-      # check if correctly predicted
-      correct += (y_hat == y).sum().item()
-
-      # collect labels for confusion matrix
-      if calc_cm:
-        y_all = np.append(y_all, y)
-        y_hat_all = np.append(y_hat_all, y_hat)
-
-      # some prints
-      if verbose:
-        if z_batches is not None:
-          print("\nlabels: {}".format(z_batches[i]))
-        print("output: {}\npred: {}\nactu: {}, \t corr: {} ".format(o.data, y_hat, y, (y_hat == y).sum().item()))
-
-  # print accuracy
-  eval_log = "Eval: correct: [{} / {}] acc: [{:.4f}] with loss: [{:.4f}]\n".format(correct, total, 100 * correct / total, eval_loss)
-  print(eval_log)
-
-  # confusion matrix
-  if calc_cm:
-    cm = confusion_matrix(y_all, y_hat_all)
-
-  # log to file
-  if logging_enabled:
-    logging.info(eval_log)
-
-  return eval_loss, (correct / total), cm
-
-
-def print_train_info(epoch, mini_batch, cum_loss, k_print=10):
-  """
-  print some training info
-  """
-
-  # print loss
-  if mini_batch % k_print == k_print-1:
-
-    # print info
-    print('epoch: {}, mini-batch: {}, loss: [{:.5f}]'.format(epoch + 1, mini_batch + 1, cum_loss / k_print))
-
-    # zero cum loss
-    cum_loss = 0.0
-
-  return cum_loss
-
-
-
-
-
-
-
+from net_handler import CnnHandler
 
 
 def init_logging(log_path):
@@ -201,35 +43,15 @@ if __name__ == '__main__':
   create_folder([cfg['ml']['plot_path'], cfg['ml']['shift_path'], cfg['ml']['metric_path'], cfg['ml']['model_path'], cfg['ml']['model_pre_path'], cfg['ml']['log_path']])
 
   # init logging
-  init_logging(log_path)
+  init_logging(cfg['ml']['log_path'])
 
 
   # create batches
-  batch_archiv = BatchArchiv(cfg['ml']['mfcc_data_files'], batch_size=cfg['ml']['batch_size'])
-
-  # --
-  # version
-  # 1: better onset detection
-  # 2: energy frame onset detection
-
-  # params for training
-  num_epochs, lr, retrain = 500, 1e-5, True
-
-
-  # select architecture
-  nn_arch = nn_architectures[1]
-
-  # pretrained model
-  #pre_trained_model_path = model_pre_path + 'conv-fstride_c-30.pth'
-  pre_trained_model_path = None
-
+  batch_archiv = BatchArchiv(cfg['ml']['mfcc_data_files'], batch_size=cfg['ml']['train_params']['batch_size'])
 
   # params
   #params = {'version_id':version_id, 'f':f, 'batch_size':batch_size, 'num_epochs':num_epochs, 'lr':lr, 'nn_arch':nn_arch, 'pre_trained_model_path':pre_trained_model_path}
 
-
-  # extract all necessary data batches
-  #x_train, y_train, x_val, y_val, x_test, y_test, x_my, y_my, z_my, classes, n_examples_class = extract_to_batches(cfg['ml']['mfcc_data_files'], f=f, batch_size=batch_size)
 
   print("classes: ", batch_archiv.classes)
 
@@ -247,43 +69,43 @@ if __name__ == '__main__':
 
 
   # param string
-  param_str = '{}_v{}_c-{}_n-{}_bs-{}_it-{}_lr-{}'.format(nn_arch, cfg['audio_dataset']['version_nr'], len(batch_archiv.classes), batch_archiv.n_examples_class, cfg['ml']['batch_size'], cfg['ml']['num_epochs'], str(lr).replace('.', 'p'))
+  param_str = '{}_v{}_c-{}_n-{}_bs-{}_it-{}_lr-{}'.format(cfg['ml']['nn_arch'], cfg['audio_dataset']['version_nr'], len(batch_archiv.classes), batch_archiv.n_examples_class, cfg['ml']['train_params']['batch_size'], cfg['ml']['train_params']['num_epochs'], str(cfg['ml']['train_params']['lr']).replace('.', 'p'))
 
 
-  if pre_trained_model_path is not None:
-    param_str = param_str + '_pre'
+  # model handler
+  cnn_handler = CnnHandler(nn_arch=cfg['ml']['nn_arch'], batch_archiv=batch_archiv) 
 
+  # load pre trained model
+  if cfg['ml']['use_pre_trained_model']:
+    cnn_handler.load_pre_trained_model(cfg['ml']['pre_trained_model_path'])
 
-  # model trainer
-
-
-  # init model
-  model = get_nn_model(nn_arch, len(classes))
-
-  # use pre-trained model
-  model = get_pretrained_model(model, pre_trained_model_path)
 
   # --
   # training
 
   # check if model already exists
-  if not os.path.exists(cfg['ml']['model_path'] + param_str + '.pth') or retrain:
+  if not os.path.exists(cfg['ml']['model_path'] + param_str + '.pth') or cfg['ml']['retrain']:
 
     # train
-    model, train_loss, val_loss, val_acc = train_nn(model, x_train, y_train, x_val, y_val, classes, nn_arch, num_epochs=cfg['ml']['num_epochs'], lr=cfg['ml']['lr'], param_str=param_str)
+    score_collecotr = cnn_handler.train_nn(num_epochs=cfg['ml']['train_params']['num_epochs'], lr=cfg['ml']['train_params']['lr'], param_str=param_str)
 
+    # training info
+    logging.info('Traning on arch: {}  time: {}'.format(param_str, s_to_hms_str(score_collector.time_usage)))
+    
     # save model
-    torch.save(model.state_dict(), model_path + param_str + '.pth')
-    if pre_trained_model_path is not None:
-      torch.save(model.state_dict(), '{}{}_c-{}{}'.format(model_pre_path, nn_arch, len(classes), '.pth'))
+    cnn_handler.save_model(cfg['ml']['model_path'] + param_str + '.pth')
+
+    # save as pre trained model as well
+    if cfg['ml']['save_model_as_pre_model']:
+      cnn_handler.save_model('{}{}_c-{}{}'.format(cfg['ml']['pre_trained_model_path'], cnn_handler.nn_arch, len(cnn_handler.batch_archiv.classes), '.pth'))
 
     # save infos
-    np.savez(model_path + param_str + '.npz', params=params, param_str=param_str, class_dict=batches.class_dict, model_file_path=model_path + param_str + '.pth')
-    np.savez(metric_path + 'metrics_' + param_str + '.npz', train_loss=train_loss, val_loss=val_loss, val_acc=val_acc)
+    np.savez(cfg['ml']['model_path'] + param_str + '.npz', params=params, param_str=param_str, class_dict=batches.class_dict, model_file_path=model_path + param_str + '.pth')
+    np.savez(cfg['ml']['metric_path'] + 'metrics_' + param_str + '.npz', score_collector=score_collector)
 
     # plots
-    plot_train_loss(train_loss, val_loss, plot_path, name=param_str + '_train_loss')
-    plot_val_acc(val_acc, plot_path, name=param_str + '_val_acc')
+    plot_train_loss(score_collector.train_loss, score_collector.val_loss, cfg['ml']['plot_path'], name=param_str + '_train_loss')
+    plot_val_acc(score_collector.val_acc, cfg['ml']['plot_path'], name=param_str + '_val_acc')
 
   # load model params from file
   else:
@@ -301,14 +123,27 @@ if __name__ == '__main__':
   print("\n--Evaluation on Test Set:")
 
   # activate eval mode (no dropout layers)
-  model.eval()
+  cnn_handler.model.eval()
 
   # evaluation of model
-  eval_loss, acc, cm = eval_nn(model, x_test, y_test, classes, calc_cm=True)
-  print("confusion matrix:\n", cm)
+  eval_score = cnn_handler.eval_nn(cnn_handler.model, calc_cm=True)
+
+  # print accuracy
+  eval_log = eval_score.info_log()
+
+
+  # --
+  # info output
+
+  # log to file
+  if logging_enabled:
+    logging.info(eval_log)
+
+  # print confusion matrix
+  print("confusion matrix:\n", eval_score.cm)
 
   # plot confusion matrix
-  plot_confusion_matrix(cm, classes, plot_path=plot_path, name=param_str + '_confusion_test')
+  plot_confusion_matrix(eval_score.cm, batch_archiv.classes, plot_path=cfg['ml']['plot_path'], name=param_str + '_confusion_test')
 
 
   # --
@@ -318,12 +153,14 @@ if __name__ == '__main__':
     print("\n--Evaluation on My Set:")
 
     # evaluation of model
-    eval_loss, acc, cm = eval_nn(model, x_my, y_my, classes, z_batches=z_my, calc_cm=True, verbose=True)
-    print("confusion matrix:\n", cm)
+    eval_score = eval_nn(model, x_my, y_my, classes, z_batches=batch_archiv.z_my, calc_cm=True, verbose=True)
+    print("confusion matrix:\n", eval_score.cm)
 
     # plot confusion matrix
-    plot_confusion_matrix(cm, classes, plot_path=plot_path, name=param_str + '_confusion_my')
+    plot_confusion_matrix(eval_score.cm, batch_archiv.classes, plot_path=cfg['ml']['plot_path'], name=param_str + '_confusion_my')
 
+
+  # show all plots
   plt.show()
 
 
--- net_handler.py
+++ net_handler.py
@@ -5,100 +5,107 @@ Handling Neural Networks
 import numpy as np
 import torch
 
-from sklearn.metrics import confusion_matrix
+from conv_nets import *
 
 
 class NetHandler():
-  """
-  Neural Network Handler
-  """
+	"""
+	Neural Network Handler
+	"""
 
-  def __init__(self, nn_arch, batch_archiv, pre_trained_model_path=None):
+	def __init__(self, nn_arch, batch_archiv):
 
-  	# neural net architecture (see in config which are available : string)
-  	self.nn_arch = nn_arch
+		# neural net architecture (see in config which are available : string)
+		self.nn_arch = nn_arch
 
-  	# training set
-  	self.batch_archiv = batch_archiv
+		# training set
+		self.batch_archiv = batch_archiv
 
-  	# pre trained models
-  	self.pre_trained_model_path = pre_trained_model_path
-
-  	# neural network model
-  	self.model = get_nn_model()
+		# neural network model
+		self.model = get_nn_model()
 
 
 	def get_nn_model(self):
+		"""
+		simply get the desired nn model
+		"""
+
+		# select network architecture
+		if self.nn_arch == 'conv-trad':
+
+		  # traditional conv-net
+		  self.model = ConvNetTrad(batch_archiv.n_classes)
+
+		elif self.nn_arch == 'conv-fstride':
+
+		  # limited multipliers conv-net
+		  self.model = ConvNetFstride4(batch_archiv.n_classes)
+
+		# did not find architecture
+		else:
+
+		  print("Network Architecture not found, uses: conf-trad")
+
+		  # traditional conv-net
+		  self.model = ConvNetTrad(batch_archiv.n_classes)
+
+
+	def load_model(self, model_path):
 	  """
-	  simply get the desired nn model
-	  """
-
-	  # select network architecture
-	  if self.nn_arch == 'conv-trad':
-
-	    # traditional conv-net
-	    self.model = ConvNetTrad(batch_archiv.n_classes)
-
-	  elif self.nn_arch == 'conv-fstride':
-
-	    # limited multipliers conv-net
-	    self.model = ConvNetFstride4(batch_archiv.n_classes)
-
-	  # did not find architecture
-	  else:
-
-	    print("Network Architecture not found, uses: conf-trad")
-
-	    # traditional conv-net
-	    self.model = ConvNetTrad(batch_archiv.n_classes)
-
-
-	def get_pretrained_model(self, pre_trained_model_path):
-	  """
-	  get pretrained model
+	  load model
 	  """
 
 	  # same model
-	  if self.pre_trained_model_path is None:
+	  if model_path is None:
 	    return
 
 	  # load model
 	  try:
-	    print("load model: ", pre_trained_model_path)
-	    self.model.load_state_dict(torch.load(pre_trained_model_path))
+	    print("load model: ", model_path)
+	    self.model.load_state_dict(torch.load(model_path))
 
 	  except:
 	    print("could not load pre-trained model!!!")
 
 
+	def save_model(self, model_path):
+		"""
+		saves model
+		"""
+
+		# just save the model
+		torch.save(self.model.state_dict(), model_path)
+
+
 	def print_train_info(self, epoch, mini_batch, cum_loss, k_print=10):
-	  """
-	  print some training info
-	  """
+		"""
+		print some training info
+		"""
 
-	  # print loss
-	  if mini_batch % k_print == k_print-1:
+		# print loss
+		if mini_batch % k_print == k_print-1:
 
-	    # print info
-	    print('epoch: {}, mini-batch: {}, loss: [{:.5f}]'.format(epoch + 1, mini_batch + 1, cum_loss / k_print))
+		  # print info
+		  print('epoch: {}, mini-batch: {}, loss: [{:.5f}]'.format(epoch + 1, mini_batch + 1, cum_loss / k_print))
 
-	    # zero cum loss
-	    cum_loss = 0.0
+		  # zero cum loss
+		  cum_loss = 0.0
 
-	  return cum_loss
-
-  def train(self):
-  	"""
-  	train interface
-  	"""
-  	pass
+		return cum_loss
 
 
-  def eval(self):
-  	"""
-  	evaluation interface
-  	"""
-  	pass
+	def train_nn(self):
+		"""
+		train interface
+		"""
+		pass
+
+
+	def eval_nn(self):
+		"""
+		evaluation interface
+		"""
+		pass
 
 
 
@@ -107,89 +114,93 @@ class CnnHandler(NetHandler):
 	Neural Network Mentor for CNNs
 	"""
 
-  def __init__(self, nn_arch, batch_archiv):
+	def __init__(self, nn_arch, batch_archiv, pre_trained_model_path=None):
 
 		# parent class init
-		super().__init__(nn_arch, batch_archiv)
+		super().__init__(nn_arch, batch_archiv, pre_trained_model_path=None)
 
 		# loss criterion
-	  self.criterion = torch.nn.CrossEntropyLoss()
+		self.criterion = torch.nn.CrossEntropyLoss()
 
 
-  def train(self, num_epochs=2, lr=1e-3, momentum=0.5, param_str='nope'):
-  	"""
-  	train the neural network
-  	"""
+	def train_nn(self, num_epochs=2, lr=1e-3, momentum=0.5, param_str='nope'):
+		"""
+		train the neural network
+		"""
 
-	  # create optimizer
-	  #optimizer = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
-	  optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+		# create optimizer
+		#optimizer = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
+		optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
 
-	  # collect losses over epochs
-	  train_loss = np.zeros(num_epochs)
-	  val_loss = np.zeros(num_epochs)
-	  val_acc = np.zeros(num_epochs)
+		# score collector
+		score_collector = ScoreCollecor(num_epochs)
 
-	  print("\n--Training starts:")
+		print("\n--Training starts:")
 
-	  # start time
-	  start_time = time.time()
+		# start time
+		start_time = time.time()
 
-	  # epochs
-	  for epoch in range(num_epochs):
+		# epochs
+		for epoch in range(num_epochs):
 
-	    # cumulated loss
-	    cum_loss = 0.0
+		  # cumulated loss
+		  cum_loss = 0.0
 
-	    # TODO: do this with loader function from pytorch (maybe or not)
-	    # fetch data samples
-	    for i, (x, y) in enumerate(zip(x_train, y_train)):
+		  # TODO: do this with loader function from pytorch (maybe or not)
+		  # fetch data samples
+		  for i, (x, y) in enumerate(zip(x_train, y_train)):
 
-	      # zero parameter gradients
-	      optimizer.zero_grad()
+		    # zero parameter gradients
+		    optimizer.zero_grad()
 
-	      # forward pass o:[b x c]
-	      o = self.model(x)
+		    # forward pass o:[b x c]
+		    o = self.model(x)
 
-	      # loss
-	      loss = self.criterion(o, y)
+		    # loss
+		    loss = self.criterion(o, y)
 
-	      # backward
-	      loss.backward()
+		    # backward
+		    loss.backward()
 
-	      # optimizer step - update params
-	      optimizer.step()
+		    # optimizer step - update params
+		    optimizer.step()
 
-	      # loss update
-	      cum_loss += loss.item()
+		    # loss update
+		    cum_loss += loss.item()
 
-	      # batch loss
-	      train_loss[epoch] += cum_loss
+		    # batch loss
+		    score_collector.train_loss[epoch] += cum_loss
 
-	      # print some infos, reset cum_loss
-	      cum_loss = print_train_info(epoch, i, cum_loss, k_print=y_train.shape[0] // 10)
+		    # print some infos, reset cum_loss
+		    cum_loss = print_train_info(epoch, i, cum_loss, k_print=y_train.shape[0] // 10)
 
-	    # valdiation
-	    val_loss[epoch], val_acc[epoch], _ = self.eval(self.model, self.x_val, self.y_val, self.classes, logging_enabled=False)
+		  # valdiation
+		  eval_score = self.eval_nn('val')
 
-	    # TODO: Early stopping if necessary
-	    
-	  print('--Training finished')
+		  # update score collector
+		  score_collector.val_loss[epoch], score_collector.val_acc[epoch] = eval_score.loss, eval_score.acc
 
-	  # log time
-	  logging.info('Traning on arch: {}  time: {}'.format(param_str, s_to_hms_str(time.time() - start_time)))
+		  # TODO: Early stopping if necessary
+		  
+		print('--Training finished')
 
-	  return model, train_loss, val_loss, val_acc
+		# log time
+		score_collector.time_usage = time.time() - start_time 
+
+		return score_collector
 
 
-	def eval(x_eval, y_eval, classes, z_eval=None, logging_enabled=True, calc_cm=False, verbose=False):
+	def eval_nn(eval_set, calc_cm=False, verbose=False):
 	  """
 	  evaluation of nn
+	  use eval_set out of ['val', 'test', 'my']
 	  """
 
-	  # init
-	  correct, total, eval_loss, cm = 0, 0, 0.0, None
-	  y_all, y_hat_all = np.empty(shape=(0), dtype=y_eval.numpy().dtype), np.empty(shape=(0), dtype=y_eval.numpy().dtype)
+	  # init score
+	  eval_score = EvalScore(label_dtype=batch_archiv.y_eval.numpy().dtype, calc_cm=calc_cm)
+
+	  # select the evaluation set
+	  x_eval, y_eval, z_eval = eval_select_set(eval_set)
 
 	  # no gradients for eval
 	  with torch.no_grad():
@@ -201,21 +212,13 @@ class CnnHandler(NetHandler):
 	      o = self.model(x)
 
 	      # loss
-	      eval_loss += self.criterion(o, y)
+	      loss = self.criterion(o, y)
 
 	      # prediction
 	      _, y_hat = torch.max(o.data, 1)
 
-	      # add total amount of prediction
-	      total += y.size(0)
-
-	      # check if correctly predicted
-	      correct += (y_hat == y).sum().item()
-
-	      # collect labels for confusion matrix
-	      if calc_cm:
-	        y_all = np.append(y_all, y)
-	        y_hat_all = np.append(y_hat_all, y_hat)
+	      # update eval score
+	      eval_score.update(loss, y, y_hat)
 
 	      # some prints
 	      if verbose:
@@ -223,24 +226,158 @@ class CnnHandler(NetHandler):
 	          print("\nlabels: {}".format(z_eval[i]))
 	        print("output: {}\npred: {}\nactu: {}, \t corr: {} ".format(o.data, y_hat, y, (y_hat == y).sum().item()))
 
-	  # print accuracy
-	  eval_log = "Eval: correct: [{} / {}] acc: [{:.4f}] with loss: [{:.4f}]\n".format(correct, total, 100 * correct / total, eval_loss)
-	  print(eval_log)
+	  # finish up scores
+	  eval_score.finish()
 
-	  # confusion matrix
-	  if calc_cm:
-	    cm = confusion_matrix(y_all, y_hat_all)
-
-	  # log to file
-	  if logging_enabled:
-	    logging.info(eval_log)
-
-	  return eval_loss, (correct / total), cm
+	  return eval_score
 
 
+	def eval_select_set(self, eval_set):
+		"""
+		select set to evaluate
+		"""
+
+		# select the set
+		x_eval, y_eval, z_eval = None, None, None
+
+		if eval_set == 'val':
+			x_eval, y_eval, z_eval = batch_archiv.x_val, batch_archiv.y_val, None
+
+		elif eval_set == 'test':
+			x_eval, y_eval, z_eval = batch_archiv.x_test, batch_archiv.y_test, None
+
+		# determine evaluation files
+		elif eval_set == 'my':
+			x_eval, y_eval, z_eval = batch_archiv.x_my, batch_archiv.y_my, batch_archiv.z_my
+
+		else:
+			print("wrong usage of eval_nn, select eval_set one out of ['val', 'test', 'my']")
+
+		return x_eval, y_eval, z_eval
+
+
+
+class ScoreCollecor():
+	"""
+	collection of scores
+	"""
+
+	def __init__(self, num_epochs):
+
+		# collect losses over epochs
+		self.train_loss = np.zeros(num_epochs)
+		self.val_loss = np.zeros(num_epochs)
+		self.val_acc = np.zeros(num_epochs)
+
+		# time score
+		self.time_usage = None
+
+
+
+class EvalScore():
+	"""
+	typical scores for evaluation
+	"""
+
+	def __init__(self, label_dtype, calc_cm=False):
+
+		# params
+		self.label_dtype = label_dtype
+		self.calc_cm = calc_cm
+
+		# init vars
+		self.loss = 0.0
+		self.correct = 0
+		self.total = 0
+		self.cm = None
+		self.acc = None
+
+		# all labels from batches
+		self.y_all = np.empty(shape=(0), dtype=self.label_dtype)
+
+		# all predicted labels
+		self.y_hat_all = np.empty(shape=(0), dtype=self.label_dtype)
+
+
+	def update(self, loss, y, y_hat):
+		"""
+		update score (batch update)
+		"""
+
+		# loss update
+		self.loss += loss
+
+		# add total amount of prediction
+		self.total += y.size(0)
+
+		# check if correctly predicted
+		self.correct += (y_hat == y).sum().item()
+
+		# collect labels for confusion matrix
+		if self.calc_cm:
+			self.y_all = np.append(self.y_all, y)
+			self.y_hat_all = np.append(self.y_hat_all, y_hat)
+
+
+	def finish(self):
+		"""
+		finishing procedure
+		"""
+
+		from sklearn.metrics import confusion_matrix
+
+		# no cm was required
+		if not self.calc_cm:
+			return
+
+		# confusion matrix
+		self.cm = confusion_matrix(self.y_all, self.y_hat_all)
+
+		# accuracy
+		self.acc = 100 * self.correct / self.total
+
+
+	def info_log(self, do_print=True):
+		"""
+		print evaluation outcome
+		"""
+
+		# message
+		eval_log = "Eval: correct: [{} / {}] acc: [{:.4f}] with loss: [{:.4f}]\n".format(self.correct, self.total, self.acc, self.loss)
+
+		# print
+		if do_print:
+			print(eval_log)
+
+		return eval_log
 
 
 
 
 
 
+if __name__ == '__main__':
+	"""
+		handles all neural networks with training and evaluation 
+	"""
+
+	import yaml
+	from batch_archiv import BatchArchiv
+
+	# yaml config file
+	cfg = yaml.safe_load(open("./config.yaml"))
+
+	# create batches
+	batch_archiv = BatchArchiv(cfg['ml']['mfcc_data_files'], batch_size=32, batch_size_eval=4)
+
+	# select architecture
+	nn_arch = 'conv-fstride'
+
+	# create an cnn handler
+	cnn_handler = CnnHandler(nn_arch, batch_archiv)
+
+	# training
+	cnn_handler.train_nn(num_epochs=2, lr=1e-3, momentum=0.5, param_str='nope')
+
+	# validation
+	cnn_handler.eval_nn(calc_cm=False, verbose=False, use_my_eval=False)
