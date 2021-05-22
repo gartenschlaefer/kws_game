52228334ba19621dd898fe24e35037c108c7c7d6
Christian <christian.walter@outlook.at>
2021-05-23 00:42:29 +0200

    wavenets layout and game loop

--- game/game_handler.py
+++ game/game_handler.py
@@ -6,8 +6,15 @@ import pygame
 
 from color_bag import ColorBag
 from interactable import Interactable
-from game_logic import GameLogic
+from game_logic import GameLogic, ThingsGameLogic
 from menu import MainMenu, HelpMenu, OptionMenu
+from text import Text
+from levels import Level_01, Level_02
+
+# append paths
+import sys
+sys.path.append("../")
+from capture_screen import ScreenCapturer
 
 
 class GameHandler():
@@ -15,24 +22,25 @@ class GameHandler():
   game handler
   """
 
-  def __init__(self, cfg):
+  def __init__(self, cfg, mic=None):
 
     # arguments
     self.cfg = cfg
+    self.mic = mic
 
     # init pygame
     pygame.init()
 
     # init display
-    self.screen = pygame.display.set_mode(cfg['game']['screen_size'])
+    self.screen = pygame.display.set_mode(self.cfg['game']['screen_size'])
+
+    # screen capturer
+    self.screen_capturer = ScreenCapturer(self.screen, self.cfg['game'])
 
     # menues
     self.main_menu = MainMenu(self.cfg['game'], self.screen)
     self.help_menu = HelpMenu(self.cfg['game'], self.screen)
     self.option_menu = OptionMenu(self.cfg['game'], self.screen)
-
-    # levels
-    # todo...
 
     # game loop
     self.game_loop_state_dict = {'main_menu_loop': 0, 'game_loop': 1, 'help_menu_loop': 2, 'option_menu_loop': 3, 'exit': 4}
@@ -58,7 +66,7 @@ class GameHandler():
       elif self.game_loop_state == self.game_loop_state_dict['option_menu_loop']: self.det_next_loop(self.option_menu.menu_loop())
 
       # run game loop
-      # todo...
+      elif self.game_loop_state == self.game_loop_state_dict['game_loop']: self.det_next_loop(self.game_loop())
 
       # exit
       else: self.game_loop_state = self.game_loop_state_dict['exit']
@@ -72,6 +80,52 @@ class GameHandler():
     # game loop state update
     self.game_loop_state = self.game_loop_state_dict[self.loop_action_dict[action]] if self.game_loop_state == self.game_loop_state_dict['main_menu_loop'] else self.game_loop_state_dict['main_menu_loop']
 
+
+  def game_loop(self):
+    """
+    actual game loop
+    """
+
+    # text
+    text = Text(self.screen)
+
+    # level creation
+    levels = [Level_01(self.screen, self.cfg['game']['screen_size'], self.mic), Level_02(self.screen, self.cfg['game']['screen_size'], self.mic)]
+
+    # choose level
+    level = levels[0]
+
+    # game logic with dependencies
+    game_logic = ThingsGameLogic(level, levels, text)
+
+    # add clock
+    clock = pygame.time.Clock()
+
+    # mic stream and update
+    #with self.mic.stream:
+
+    # game loop
+    while game_logic.run_loop:
+      for event in pygame.event.get():
+
+        # input handling
+        game_logic.event_update(event)
+        level.event_update(event)
+
+      # frame update
+      level = game_logic.update()
+      level.update()
+      text.draw()
+      self.screen_capturer.update()
+
+      # update display
+      pygame.display.flip()
+
+      # reduce framerate
+      clock.tick(cfg['game']['fps'])
+
+    # save video plus audio
+    self.screen_capturer.save_video(self.mic)
 
 
 if __name__ == '__main__':
--- not_relevant/tut_wavenet.py
+++ not_relevant/tut_wavenet.py
@@ -1,5 +1,5 @@
 """
-wavenets from master oord
+wavenets
 """
 
 import torch
@@ -11,7 +11,7 @@ class WavenetResBlock(nn.Module):
   wavenet residual block
   """
 
-  def __init__(self, in_channels, out_channels, dilation=1):
+  def __init__(self, in_channels, out_channels, skip_channels=16, dilation=1):
 
     # parent init
     super().__init__()
@@ -19,13 +19,15 @@ class WavenetResBlock(nn.Module):
     # arguments
     self.in_channels = in_channels
     self.out_channels = out_channels
+    self.skip_channels = skip_channels
+    self.dilation = dilation
 
     # dilated convolution filter and gate
-    self.conv_filter = nn.Conv1d(self.in_channels, self.out_channels, kernel_size=2, stride=1, padding=1, dilation=dilation, groups=1, bias=False, padding_mode='zeros')
-    self.conv_gate = nn.Conv1d(self.in_channels, self.out_channels, kernel_size=2, stride=1, padding=1, dilation=dilation, groups=1, bias=False, padding_mode='zeros')
+    self.conv_filter = nn.Conv1d(self.in_channels, self.out_channels, kernel_size=2, stride=1, padding=0, dilation=self.dilation, groups=1, bias=False, padding_mode='zeros')
+    self.conv_gate = nn.Conv1d(self.in_channels, self.out_channels, kernel_size=2, stride=1, padding=0, dilation=self.dilation, groups=1, bias=False, padding_mode='zeros')
 
     # 1 x 1 convolution for skip connection
-    self.conv_skip = nn.Conv1d(self.out_channels, out_channels=self.in_channels, kernel_size=2, stride=1, padding=0, dilation=1, groups=1, bias=False, padding_mode='zeros')
+    self.conv_skip = nn.Conv1d(self.out_channels, out_channels=self.skip_channels, kernel_size=2, stride=1, padding=0, dilation=1, groups=1, bias=False, padding_mode='zeros')
     
 
   def forward(self, x):
@@ -33,10 +35,17 @@ class WavenetResBlock(nn.Module):
     forward connection
     """
 
-    print("x: ", x.shape)
+    #print("x: ", x.shape)
+
+    # input length
+    input_len = x.shape[-1]
 
     # residual connection
     residual = x
+
+    # zero padding from left
+    x = torch.nn.functional.pad(x, (self.dilation, 0), 'constant', 0)
+    #print("x1: ", x.shape)
 
     # 1. conv layers
     o_f = torch.tanh(self.conv_filter(x))
@@ -44,17 +53,29 @@ class WavenetResBlock(nn.Module):
 
     # element-wise multiplication
     o = o_f * o_g
-    print("o1: ", o.shape)
+    #print("o1: ", o.shape)
+
+    # padding for skip connection
+    o = torch.nn.functional.pad(o, (1, 0), 'constant', 0)
+    #print("o2: ", o.shape)
 
     # skip connection
     skip = self.conv_skip(o)
     o = self.conv_skip(o)
 
-    print("o2: ", o.shape)
-    print("r: ", residual.shape)
+    #print("o3: ", o.shape)
+
+    # # keep input size
+    # if o.shape[-1] > input_len:
+    #   o = o[:, :, :-1]
+    #   skip = skip[:, :, :-1]
+    # print("o3: ", o.shape)
+    #print("r: ", residual.shape)
 
     # add residual
     o += residual
+
+
 
     return o, skip
 
@@ -63,6 +84,7 @@ class WavenetResBlock(nn.Module):
 class Wavenet(nn.Module):
   """
   wavenet class
+  receptive field: 2^n_layers
   """
 
   def __init__(self):
@@ -70,20 +92,37 @@ class Wavenet(nn.Module):
     # parent init
     super().__init__()
 
-    # wavenet blocks
-    self.wavenet_block1 = WavenetResBlock(in_channels=1, out_channels=16)
+    self.in_channels = 1
+    self.out_channels = 16
+    self.skip_channels = 16
+
+    # layers of wavenet blocks
+    self.n_layers = 8
+
+    # wavenet_layers
+    self.wavenet_layers = torch.nn.ModuleList()
+
+    # first block
+    self.wavenet_layers.append(WavenetResBlock(self.in_channels, self.out_channels, skip_channels=self.skip_channels, dilation=1))
+
+    # append further blocks
+    for i in range(1, self.n_layers): self.wavenet_layers.append(WavenetResBlock(self.out_channels, self.out_channels, skip_channels=self.skip_channels, dilation=2**i))
+
+    # # wavenet block 1
+    # self.wavenet_block1 = WavenetResBlock(self.in_channels, self.out_channels, skip_channels=self.skip_channels, dilation=1)
+    
+    # # wavenet block 2
+    # self.wavenet_block2 = WavenetResBlock(self.out_channels, self.out_channels, skip_channels=self.skip_channels, dilation=2)
+
+    # # wavenet block 3
+    # self.wavenet_block3 = WavenetResBlock(self.out_channels, self.out_channels, skip_channels=self.skip_channels, dilation=4)
+
+    # # wavenet block 4
+    # self.wavenet_block4 = WavenetResBlock(self.out_channels, self.out_channels, skip_channels=self.skip_channels, dilation=8)
 
     # conv layer post
-    self.conv_post1 = nn.Conv1d(in_channels=16, out_channels=16, kernel_size=1, stride=1, padding=0, dilation=1, groups=1, bias=False, padding_mode='zeros')
+    self.conv_post1 = nn.Conv1d(in_channels=self.skip_channels, out_channels=16, kernel_size=1, stride=1, padding=0, dilation=1, groups=1, bias=False, padding_mode='zeros')
     self.conv_post2 = nn.Conv1d(in_channels=16, out_channels=1, kernel_size=1, stride=1, padding=0, dilation=1, groups=1, bias=False, padding_mode='zeros')
-
-
-  def mu_softmax(self, x, mu=256):
-    """
-    mu softmax function
-    """
-
-    return torch.sign(x) * torch.log(1 + mu * torch.abs(x)) / torch.log(1 + mu)
 
 
   def forward(self, x):
@@ -91,11 +130,25 @@ class Wavenet(nn.Module):
     forward connection
     """
 
-    # 1. wavenet block
-    o, skip = self.wavenet_block1(x)
+    print("x: ", x.shape)
+
+    # init o
+    o = x
+
+    # init skips
+    skips = torch.zeros(x.shape[0], self.skip_channels, x.shape[-1])
+
+    # wavenet layers
+    for wavenet_layer in self.wavenet_layers: 
+
+      # wavenet layer
+      o, skip = wavenet_layer(o)
+
+      # add skips
+      skips += skip
 
     # relu of summed skip
-    x = torch.relu(skip)
+    x = torch.relu(skips)
 
     # conv layers post
     x = torch.relu(self.conv_post1(x))
@@ -107,6 +160,28 @@ class Wavenet(nn.Module):
     return x
 
 
+  def mu_softmax(self, x, mu=256):
+    """
+    mu softmax function
+    """
+
+    #print("x: ", x)
+    #x = x / torch.max(torch.abs(x))
+    #print("x norm: ", x)
+
+    x = torch.sign(x) * torch.log(1 + mu * torch.abs(x)) / torch.log(1 + torch.ones(x.shape) * mu)
+    #print("x: ", x)
+
+    return x
+
+
+  def count_params(self):
+    """
+    params count
+    """
+
+    # count
+    return [p.numel() for p in self.parameters() if p.requires_grad]
 
 
 if __name__ == '__main__':
@@ -115,12 +190,18 @@ if __name__ == '__main__':
   """
 
   # input
-  x = torch.randn(1, 1, 2)
+  x = torch.randn(1, 1, 16000)
+
+  # norm
+  x = x / torch.max(torch.abs(x))
 
   # wavenet
   wavenet = Wavenet()
+  print("wavenet: ", wavenet)
 
   # next sample
   y = wavenet(x)
 
-  print("wavenet: ", wavenet)
+  # count params
+  layer_params = wavenet.count_params()
+  print("layer: {} sum: {}".format(layer_params, sum(layer_params)))
